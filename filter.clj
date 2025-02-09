8ca026491528ac6f229bdf2394c4df0f874d1b29
Goderich <yuhsien77@gmail.com>
2025-02-09 17:45:55 +0800

    Add link buttons for publications

--- filter.clj
+++ filter.clj
@@ -4,19 +4,26 @@
             [clojure.walk :as walk]
             [clojure.string :as str]))
 
-(defn org->html-ext [fname]
-  (if (str/ends-with? fname ".org")
-    (str/replace fname #"org$" "html")
-    fname))
-
-(defn- transform-link-target
-  "If link points to an org file, change it to html."
+(defn- transform-link
+  "Filter for hyperlinks.
+  Changes links to .org files to point to .html files,
+  and adds the 'button' class to any links with specified text."
   [elem]
+  (cond
+    (str/ends-with? (get-in elem [:c 2 0]) ".org")
+    (update-in elem [:c 2 0] str/replace #"org$" "html")
+
+    (some #{(get-in elem [:c 1 0 :c])} ["PDF" "DOI" "Manuscript"])
+    (update-in elem [:c 0 1] conj "button")
+    
+    :else elem))
+
+(defn- link-filter [elem]
   (if-not (= (:t elem) "Link")
     elem
-    (update-in elem [:c 2 0] org->html-ext)))
+    (transform-link elem)))
 
 (->> (json/decode (slurp *in*) true)
-     (walk/prewalk transform-link-target)
+     (walk/prewalk link-filter)
      (json/encode)
      print)
